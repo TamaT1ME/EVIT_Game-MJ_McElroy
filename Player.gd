b8a30846f213273ee1ccd18b0b5873ab30bbfa32
MJ <goldengamer128@gmail.com>
2024-04-22 19:23:59 -0700

    Update
    built out more of the map and added new textures for tiles and some other possible future assets

--- Player.gd
+++ Player.gd
@@ -1,12 +1,44 @@
 extends CharacterBody2D
 
 var speed = 300  # speed in pixels/sec
+@onready var animations = $AnimationPlayer
+var ogDirection = ""
+var direction = ""
 
+func handleInput():
+	var moveDirection = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+	velocity = moveDirection * speed
+	
+func updateAnimation():
+	if velocity.x < 0:
+		direction = "Walk Left"
+		ogDirection = "Walk Left"
+	elif velocity.x > 0:
+		direction = "Walk Right"
+		ogDirection = "Walk Right"
+	elif velocity.y < 0:
+		direction = "Walk Up"
+		ogDirection = "Walk Up"
+	elif velocity.y > 0:
+		direction = "Walk Down"
+		ogDirection = "Walk Down"
+	else: 
+		if velocity.x == 0 && velocity.y == 0: 
+			if ogDirection == "Walk Left":
+				direction = "Idle Left"
+			elif ogDirection == "Walk Right":
+				direction = "Idle Right"
+			elif ogDirection == "Walk Up":
+				direction = "Idle Up"
+			else:
+				direction = "Idle Down"
+	
+	animations.play(direction)
+	
 func _physics_process(delta):
-	var direction = Input.get_vector("Left", "Right", "Up", "Down")
-	velocity = direction * speed
-
+	handleInput()
 	move_and_slide()
+	updateAnimation()
 
 #var speed = 300
 #
