40205415c6af20c54e5b44edfda449b8bb86ca96
MJ <goldengamer128@gmail.com>
2024-04-25 19:32:22 -0700

    update
    eyo

--- Player.gd
+++ Player.gd
@@ -34,11 +34,50 @@ func updateAnimation():
 				direction = "Idle Down"
 	
 	animations.play(direction)
-	
+
+var bistroDoorway: CollisionShape2D
+var bistroEntrance: CollisionShape2D
+var playerInRange: bool = false
+
+func _ready() -> void:
+	bistroDoorway = get_node("/root/Main/Floor_InBuildingWalls/BistroDoor/BistroDoor")
+	bistroEntrance = get_node("/root/Main/Floor_InBuildingWalls/BistroEnter/BistroEnter")
+
+func _on_bistro_door_body_entered(body: Node) -> void:
+	if "Player" in body.name:  # Assuming your player character is named "Player"
+		playerInRange = true
+
+func _on_bistro_door_body_exited(body: Node) -> void:
+	if "Player" in body.name:
+		playerInRange = false
+
+func _on_bistro_enter_body_entered(body: Node) -> void:
+	print_debug("testing")
+	if "Player" in body.name:
+		playerInRange = true
+		
+func _on_bistro_enter_body_exited(body: Node) -> void:
+	if "Player" in body.name:
+		playerInRange = false
+
+func door() -> void:
+	if bistroEntrance:
+		global_position = bistroEntrance.global_position
+	if bistroDoorway:
+		global_position = bistroDoorway.global_position
+
+func handleCollision():
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var collider = collision.get_collider()
+
 func _physics_process(delta):
 	handleInput()
 	move_and_slide()
+	handleCollision()
 	updateAnimation()
+	if Input.is_action_pressed("ui_accept") and playerInRange:
+		door()
 
 #var speed = 300
 #
@@ -47,3 +86,8 @@ func _physics_process(delta):
 		#$AnimationPlayer.play("Walk Up")
 		#velocity
 	#move_and_slide()
+
+
+func _on_hurt_box_area_entered(area):
+	if area.name == "hitBox":
+		print_debug(area.get_parent().name)
